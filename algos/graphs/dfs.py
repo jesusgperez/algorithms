59dd19473bc480ea6ee10713c5e49f9c4f3f391f
Jesus Perez <jesusgperez16@gmail.com>
2025-10-06 18:57:06 -0500

    find cycle implementation

--- algos/graphs/dfs.py
+++ algos/graphs/dfs.py
@@ -54,25 +54,47 @@ def create_undirected(n_vertices, edges):
 
     return adj
 
-
 undirected = create_undirected(6, [[1,3],[1,2],[2,5],[3,4],[4,5],[2,3],[3,5]])
 
 vertext_info, time = defaultdict(VertexInfo), 0
 
+finished = False
+
+def find_path(start, end):
+    if start == end or vertex_info[end].parent == -1:
+        print(start)
+    else:
+        find_path(start, vertex_info[end].parent)
+        print(end)
+
+def process_edge(x, y):
+    global finished
+
+    if vertex_info[y].parent != x:
+        finished = True
+        find_path(y, x)
+
 def dfs(vertex: int, adj: List[List]): # undirected search
-    global time
+    global time, finished
     time += 1
     vertex_info[vertex].discovered = True
     vertex_info[vertex].entry = time
+
+    if finished: return
 
     for child in adj[vertex]:
         if not vertex_info[child].discovered:
             vertex_info[child].parent = vertex
             process_edge(vertex, child)
             dfs(child, adj)
-        # elif not vertex_info[child].processed and vertex_info[vertex].parent != child: # undirected edge process validation without processing coming back
-        elif not vertex_info[child].processed: # undirected edge process validation, processing coming back edge
+        # undirected edge process validation first time
+        elif not vertex_info[child].processed and vertex_info[vertex].parent != child: 
             process_edge(vertex, child)
+
+        if finished: return
+        # second time 
+        # if vertex_info[child].processed:
+        #     process_edge(vertex, child)
 
     time += 1
     vertex_info[vertex].processed = True
