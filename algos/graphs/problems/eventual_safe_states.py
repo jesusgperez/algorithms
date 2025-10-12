4c574307fcfdf21dfe3840e347e55f078d8a73c6
Jesus Perez <jesusgperez16@gmail.com>
2025-10-12 16:43:00 -0500

    Safe paths solution

--- algos/graphs/problems/eventual_safe_states.py
+++ algos/graphs/problems/eventual_safe_states.py
@@ -1,36 +1,30 @@
-from typing import List
+# https://leetcode.com/problems/find-eventual-safe-states
 
-
-# graph = [[1],[2,3],[5],[0],[5],[],[]]
+graph = [[1],[2,3],[5],[0],[5],[],[]]
 # graph = [[1,2,3,4],[1,2],[3,4],[0,4],[]]
-graph = [[],[0,2,3,4],[3],[4],[]]
+# graph = [[],[0,2,3,4],[3],[4],[]]
 
 discovered, processed = set(), set()
-terminals = {v for v in range(len(graph)) if not graph[v]}
-cycles, points_terminal = set(), set()
+cycles = set()
 safe = set()
 
 def dfs(v: int):
     discovered.add(v)
-
     is_terminal = True
 
     for u in graph[v]:
-        if u not in points_terminal:
-            is_terminal = False
-
         if u not in discovered:
             dfs(u)
         elif u not in processed:
             cycles.add(v)
 
+        if u not in safe:
+            is_terminal = False
+
         if u in cycles:
             cycles.add(v)
 
-    if is_terminal:
-        points_terminal.add(v)
-
-    if not v in cycles and points_terminal:
+    if not v in cycles and is_terminal:
         safe.add(v)
 
     processed.add(v)
