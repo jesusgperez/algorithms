4451d997b79c67e09ed91be3288baa8746af2932
Jesus Perez <jesusgperez16@gmail.com>
2025-10-17 17:41:05 -0500

    Finding circles with 1 inequality approach

--- algos/graphs/problems/equality_equations.py
+++ algos/graphs/problems/equality_equations.py
@@ -2,51 +2,70 @@ from typing import List
 from collections import defaultdict
 
 
-equations = ['a==b', 'a!=b']
-adj = defaultdict(list)
-
-
-for equation in equations:
-    if '==' in equation:
-        x, y = equation.split('==')
-        adj[x].append((y,'=='))
-        adj[y].append((x,'=='))
-    else:
-        x, y = equation.split('!=')
-        adj[x].append((y,'!='))
-        adj[y].append((x,'!='))
-
-
-edges, parents = defaultdict(str), defaultdict(lambda: -1)
-discovered, processed = set(), set()
+equations = ['a==b', 'b!=c', 'a!=c', 'c==d', 'e==b', 'd!=e', 'e==f', 'b==f', 'c!=g', 'd==g']
+adj = defaultdict(dict)
+letters = set()
 
 
 class ContradictionException(Exception):
     pass
 
 
-def process_edge(v: str, u: str, rel: str):
-    if (v,u) not in edges or edges[(v,u)] == rel: 
-        edges[(v,u)] = rel
-        edges[(u,v)] = rel
-        return
+def add_edge(equation: str, rel: str):
+    a, b = equation.split(rel)
+    letters.add(a)
+    letters.add(b)
+    if b in adj[a] or a in adj[b]:
+        raise ContradictionException
+    adj[a][b] = rel
+    adj[b][a] = rel
 
-    raise ContradictionException('')
+
+for equation in equations:
+    if '==' in equation:
+        add_edge(equation, '==')
+    else:
+        add_edge(equation, '!=')
 
 
-def dfs(v: str):
+parents = defaultdict(lambda: -1)
+discovered, processed = set(), set()
+differences = defaultdict(int)
+
+def find_path(start:str, end: str):
+    if start == end or parents[end] == -1:
+        print(end)
+    else:
+        find_path(start, parents[end])
+        print(end)
+
+
+def dfs(v: str, diff: int):
     discovered.add(v)
 
-    for u, rel in adj[v]:
+    for u, rel in adj[v].items():
         if u not in discovered:
+            diff += (rel == '!=')
+            differences[u] = diff - differences[v]
             parents[u] = v
             discovered.add(u)
-            process_edge(v, u, rel)
-            dfs(u)
-        elif u not in processed and parents[v] != u:
-            process_edge(v, u, rel)
+            dfs(u, diff)
+            diff -= (rel == '!=')
+        elif not processed and parents[v] != u:
+            if diff == 0 and rel == '!=':
+                raise ContradictionException('')
 
     processed.add(v)
 
+has_contradiction = False
 
-dfs('a')
+for l in letters:
+    if l not in discovered:
+        try:
+            dfs('a', 0)
+        except ContradictionException as e:
+            has_contradiction = True
+            break
+
+
+print(has_contradiction)
