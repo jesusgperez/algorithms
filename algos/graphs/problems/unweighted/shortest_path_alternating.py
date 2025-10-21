24a97e878ee002d91b0ea8dfba84d4f2602fb01b
Jesus Perez <jesusgperez16@gmail.com>
2025-10-21 10:57:44 -0500

    Compacting possible solution

--- algos/graphs/problems/unweighted/shortest_path_alternating.py
+++ algos/graphs/problems/unweighted/shortest_path_alternating.py
@@ -5,42 +5,39 @@ red = [[0,1],[1,2],[2,3],[3,4]]
 blue = [[0,1],[1,2],[3,3]]
 red = [[0,1],[1,2],[2,3],[3,4]]
 blue = [[1,2],[2,3],[3,1]]
+red = [[0,1],[1,2]]
+blue = [[1,0]]
 
-adj_red = defaultdict(list)
-adj_blue = defaultdict(list)
+adj = defaultdict(lambda: defaultdict(list))
 
-def create_graph(adj: Dict, edges: List[List[int]]):
-    for x, y in edges:
-        adj[x].append(y)
+def create_graph(blue: List[List[int]], red: List[List[int]]):
+    for x, y in blue:
+        adj[x]['blue'].append(y)
 
-create_graph(adj_red, red)
-create_graph(adj_blue, blue)
+    for x, y in red:
+        adj[x]['red'].append(y)
 
-def pick_adj(adj: List[List[int]]):
-    if adj == adj_red:
-        return adj_blue
-    return adj_red
+create_graph(blue, red)
 
-def pick_discovered(disc):
-    if disc == blue_discovered:
-        return red_discovered
-    return blue_discovered
+def pick_color(color: str):
+    if color == 'red':
+        return 'blue'
+    return 'red'
 
 paths = [-1 if i != 0 else i for i in range(5)]
 
-def dfs(v: int, adj: List[List[int]], disc: set, length: int):
-    disc.add(v)
+def dfs(v: int, color: str, length: int):
 
-    for u in adj[v]:
-        if u not in disc:
+    for u in adj[v][color]:
+        if (v,u) not in discovered[color]:
+            discovered[color].add((v,u))
             paths[u] = length if paths[u] == -1  else min(paths[u], length)
-            dfs(u, pick_adj(adj), pick_discovered(disc), length + 1)
+            dfs(u, pick_color(color), length + 1)
 
-blue_discovered = set()
-red_discovered = set()
-dfs(0, adj_blue, red_discovered, 1)
-blue_discovered = set()
-red_discovered = set()
-dfs(0, adj_red, blue_discovered, 1)
+discovered = defaultdict(set)
+dfs(0, 'blue', 1)
+
+discovered = defaultdict(set)
+dfs(0, 'red', 1)
 
 print(paths)
