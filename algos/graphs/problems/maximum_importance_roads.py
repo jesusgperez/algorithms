1947f4375746b4091e66a3fcd126fa5cbdad61cf
Jesus Perez <jesusgperez16@gmail.com>
2025-10-24 14:23:25 -0500

    Self: solution for maximum importance roads

--- algos/graphs/problems/maximum_importance_roads.py
+++ algos/graphs/problems/maximum_importance_roads.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from algos.graphs.utils import visualize_undirected
 
 edges = [[0,18],[18,1],[1,19],[21,20],[20,7],[7,8],[3,22],[22,13],[13,1],[13,9],[9,16],[16,7],[24,17],[17,3],[3,21],[22,6],[6,11],[11,7],[23,4],[4,15],[15,12],[5,7],[7,21],[21,1]]
-visualize_undirected(edges)
+# visualize_undirected(edges)
 n = max((v for edge in edges for v in edge))
 adj = defaultdict(list)
 weight = {}
@@ -12,27 +12,10 @@ for x,y in edges:
     adj[x].append(y)
     adj[y].append(x)
 
-discovered = set()
-def dfs(v: int, component: List):
-    discovered.add(v)
-    component.append(v)
-    for u in adj[v]:
-        if u not in discovered:
-            dfs(u, component)
-    return component
-
-components: List[List[int]] = []
-for v in range(n):
-    if v not in discovered:
-        components.append(dfs(v, []))
-
-components.sort(key= lambda x: len(x), reverse=True)
 current = n + 1
-for component in components:
-    component.sort(key=lambda x: len(adj[x]), reverse=True)
-    for v in component:
-        weight[v] = current
-        current -= 1
+for v in sorted(range(n + 1), key=lambda x: len(adj[x]), reverse=True):
+    weight[v] = current
+    current -= 1
 
 parents = defaultdict(lambda: -1)
 discovered, processed = set(), set()
