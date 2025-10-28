cb22f4ca18631b9bc87c91c92a72b7cd9ab8ffe4
Jesus Perez <jesusgperez16@gmail.com>
2025-10-28 09:02:22 -0500

    Self: correct solution for this one

--- algos/graphs/problems/closest_node_two_nodes.py
+++ algos/graphs/problems/closest_node_two_nodes.py
@@ -1,14 +1,15 @@
-from algos.graphs.utils import visualize_directed
+# from algos.graphs.utils import visualize_directed
 
 edges = [-1,7,15,15,-1,4,16,2,16,7,11,6,10,4,9,1,14,-1]
+edges = [4,4,8,-1,9,8,4,4,1,1]
 
-visualize_directed([[u, v] for u, v in enumerate(edges) if v != -1])
-node1, node2 = 3, 2
+# visualize_directed([[u, v] for u, v in enumerate(edges) if v != -1])
+node1, node2 = 6, 5
 
 if node1 == node2:
     print(node1)
 
-path1, path2 = {}, {}
+path1, path2 = {node1: 0}, {node2: 0}
 def dfs(v: int, path: set, length: int):
     discovered.add(v)
 
@@ -31,11 +32,10 @@ if node2 in path1 and node1 in path2:
     else:
         print(min(node1, node2))
 
-min_length, min_node = (len(edges), len(edges)), -1
+min_length, min_node = 2 * len(edges), -1
 for v in range(len(edges)):
-    if (v in path1 and v in path2
-    and path1[v] < min_length[0] and path2[v] < min_length[1]):
-        min_length = (path1[v], path2[v])
+    if v in path1 and v in path2 and max(path1[v],path2[v]) < min_length:
+        min_length = max(path1[v],path2[v])
         min_node = v
 
 print(min_node)
